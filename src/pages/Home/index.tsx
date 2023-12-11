46178d89a5614a336ce4e338838936d22471d082
pkolt <pkoltyshev@gmail.com>
2023-12-11 22:33:12 +0400

    Fix order image list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,26 +2,32 @@ import { Link } from 'react-router-dom';
 import { PageUrl } from '@/constants/urls';
 import { Page } from '@/components/Page';
 import { useImageStore } from '@/store/images/useImagesStore';
+import { useMemo } from 'react';
 
 const Home = () => {
   const { images } = useImageStore();
+  const orderedImages = useMemo(() => images.sort((a, b) => b.updatedAt - a.updatedAt), [images]);
   return (
     <Page title="Pixel Editor">
       <main className="d-flex flex-column flex-grow-1 justify-content-center align-items-center">
-        {images.length > 0 && (
+        {orderedImages.length > 0 && (
           <div className="mb-3">
-            <h3 className="text-center">Open</h3>
+            <h2 className="text-center">Open</h2>
             <ul className="list-group list-group-flush mb-3">
-              {images.map((it) => {
+              {orderedImages.map((it) => {
                 const url = PageUrl.Editor.replace(':id', it.id);
                 return (
-                  <li className="list-group-item btn-link">
+                  <li key={it.id} className="list-group-item btn-link">
                     <Link to={url}>{it.name}</Link>
                   </li>
                 );
               })}
             </ul>
-            <h3 className="text-center">Or</h3>
+            <h5 className="d-flex gap-3 align-items-center">
+              <hr className="flex-grow-1" />
+              Or
+              <hr className="flex-grow-1" />
+            </h5>
           </div>
         )}
         <Link to={PageUrl.CreateImage} className="btn btn-primary btn-lg">
