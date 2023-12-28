3684740e337f3d53854a8aa3dead62c6a79dd7f0
pkolt <pkoltyshev@gmail.com>
2023-12-28 20:38:28 +0400

    Fix output format

--- src/components/PixelEditor/utils.ts
+++ src/components/PixelEditor/utils.ts
@@ -4,7 +4,24 @@ import { Bitmap } from '@/utils/bitmap';
 export const imageToProgramCode = (image: ImageEntity): string => {
   const bitmap = new Bitmap(image.width, image.height, image.data);
   const data = bitmap.toSSD1306();
-  const commentCode = `// ${image.name} (${bitmap.width}x${bitmap.height})`;
-  const imgCode = `const uint8_t data[${data.length}] PROGMEM = {${Array.from(data).map((value) => `0x${value.toString(16)}`).join(', ')}};`;
-  return `${commentCode}\n${imgCode}`;
+
+  const name = image.name.replace(/\s/g, '_').toLowerCase();
+  const headerName = `${name.toUpperCase()}_H`;
+
+  const sizes = `0x${(image.width).toString()} /* width */, 0x${(image.height).toString()} /* height */`;
+  const uint8Array = Array.from(data).map((value) => `0x${value.toString(16)}`).join(', ');
+  const bitmapCode = `const uint8_t ${name}[${data.length}] PROGMEM = { ${sizes}, ${uint8Array} };`;
+
+  const output = `
+// ${name}.h (${image.width}x${image.height})
+#ifndef ${headerName}
+#define ${headerName}
+#include <stdint.h>
+#include <avr/pgmspace.h>
+
+${bitmapCode}
+
+#endif // ${headerName}
+`.replace(/^\n/, '').replace(/\n$/, '');
+  return output;
 };
