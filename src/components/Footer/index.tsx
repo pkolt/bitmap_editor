8ce5a7207e38bdbba7de76c951a14d47d547d17f
pkolt <pkoltyshev@gmail.com>
2024-01-25 21:30:17 +0400

    Fix link

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   const year = DateTime.local().year;
   return (
     <footer className="container-fluid p-3 bg-light text-center rounded-top-2">
-      Created by <a href="https://pkolt.ru">Pavel Koltyshev</a> &copy; 2023-{year}
+      Created by <a href="https://github.com/pkolt">Pavel Koltyshev</a> &copy; 2023-{year}
     </footer>
   );
 };
