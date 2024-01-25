25ff61eaa000aaa8576b39a732f59b48380f24b7
pkolt <pkoltyshev@gmail.com>
2024-01-25 20:28:06 +0400

    Fix footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,10 @@
+import { DateTime } from 'luxon';
+
 export const Footer = () => {
+  const year = DateTime.local().year;
   return (
     <footer className="container-fluid p-3 bg-light text-center rounded-top-2">
-      Created by <a href="https://pkolt.ru">Pavel Koltyshev</a> &copy; 2023
+      Created by <a href="https://pkolt.ru">Pavel Koltyshev</a> &copy; 2023-{year}
     </footer>
   );
 };
