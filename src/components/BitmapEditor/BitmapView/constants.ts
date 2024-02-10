66cbfa2edda27122002d27cd5a90f19f9cd8bbfe
pkolt <pkoltyshev@gmail.com>
2024-02-10 12:09:33 +0400

    Fix refactoring bitmap view

---
+++ src/components/BitmapEditor/BitmapView/constants.ts
@@ -0,0 +1,8 @@
+// All sizes in pixels
+export const SQUARE_SIZE = 10;
+export const SQUARE_COLOR = '#000';
+export const SEPARATOR_COLOR = '#777';
+export const SEPARATOR_SIZE = 1;
+export const BORDER_SIZE = 1;
+export const BORDER_COLOR = '#ccc';
+export const STEP_SIZE = SQUARE_SIZE + BORDER_SIZE;
--- src/components/BitmapEditor/BitmapView/index.tsx
+++ src/components/BitmapEditor/BitmapView/index.tsx
@@ -1,105 +1,9 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import styles from './index.module.css';
 import { Bitmap } from '@/utils/bitmap';
-import { useSettingsStore, type GridSettings } from '@/store/settings/useSettingsStore';
-
-// All sizes in pixels
-const SQUARE_SIZE = 10;
-const SQUARE_COLOR = '#000';
-const SEPARATOR_COLOR = '#777';
-const SEPARATOR_SIZE = 1;
-const BORDER_SIZE = 1;
-const BORDER_COLOR = '#ccc';
-const STEP_SIZE = SQUARE_SIZE + BORDER_SIZE;
-
-type CanvasContext = CanvasRenderingContext2D;
-
-interface Sizes {
-  canvasWidth: number;
-  canvasHeight: number;
-  bitmapWidth: number;
-  bitmapHeight: number;
-}
-
-const getCanvasSize = (bitmapSize: number) => bitmapSize * SQUARE_SIZE + (bitmapSize + 1) * BORDER_SIZE;
-
-const clearDisplay = (ctx: CanvasContext, sizes: Sizes): void => {
-  ctx.clearRect(0, 0, sizes.canvasWidth, sizes.canvasHeight);
-};
-
-const drawGrid = (ctx: CanvasContext, sizes: Sizes): void => {
-  const { canvasWidth, canvasHeight, bitmapWidth, bitmapHeight } = sizes;
-
-  ctx.strokeStyle = BORDER_COLOR;
-  ctx.lineWidth = BORDER_SIZE;
-
-  for (let i = 0; i <= bitmapHeight; i++) {
-    const y = i * STEP_SIZE;
-    ctx.beginPath();
-    ctx.moveTo(0, y);
-    ctx.lineTo(canvasWidth, y);
-    ctx.stroke();
-  }
-
-  for (let i = 0; i <= bitmapWidth; i++) {
-    const x = i * STEP_SIZE;
-    ctx.beginPath();
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, canvasHeight);
-    ctx.stroke();
-  }
-};
-
-const drawBitmap = (ctx: CanvasContext, bitmap: Bitmap): void => {
-  const bitmapWidth = bitmap.width;
-  const bitmapHeight = bitmap.height;
-
-  ctx.fillStyle = SQUARE_COLOR;
-
-  let index = 0;
-  for (let y = 0; y < bitmapHeight; y++) {
-    for (let x = 0; x < bitmapWidth; x++) {
-      const isFill = bitmap.getByIndex(index);
-      if (isFill) {
-        const posX = x * STEP_SIZE + BORDER_SIZE;
-        const posY = y * STEP_SIZE + BORDER_SIZE;
-        ctx.fillRect(posX, posY, SQUARE_SIZE, SQUARE_SIZE);
-      }
-      index++;
-    }
-  }
-};
-
-const drawSeparators = (ctx: CanvasContext, sizes: Sizes, grid: GridSettings): void => {
-  const { canvasWidth, canvasHeight, bitmapWidth, bitmapHeight } = sizes;
-
-  ctx.strokeStyle = SEPARATOR_COLOR;
-  ctx.lineWidth = SEPARATOR_SIZE;
-
-  if (grid.visibleRows) {
-    for (let i = 0; i <= bitmapHeight; i++) {
-      if (i % grid.rowSize === 0 && i > 0 && i < bitmapHeight) {
-        const y = i * STEP_SIZE;
-        ctx.beginPath();
-        ctx.moveTo(0, y);
-        ctx.lineTo(canvasWidth, y);
-        ctx.stroke();
-      }
-    }
-  }
-
-  if (grid.visibleColumns) {
-    for (let i = 0; i <= bitmapWidth; i++) {
-      if (i % grid.columnSize === 0 && i > 0 && i < bitmapWidth) {
-        const x = i * STEP_SIZE;
-        ctx.beginPath();
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, canvasHeight);
-        ctx.stroke();
-      }
-    }
-  }
-};
+import { useSettingsStore } from '@/store/settings/useSettingsStore';
+import { Sizes } from './types';
+import { clearDisplay, drawBitmap, drawPointsBorders, drawGrid, getCanvasSize } from './utils';
 
 interface BitmapViewProps {
   bitmap: Bitmap;
@@ -110,7 +14,7 @@ interface BitmapViewProps {
 export const BitmapView = ({ bitmap, onChangeBitmap, eraser }: BitmapViewProps): JSX.Element => {
   const { grid } = useSettingsStore();
   const [sizes, setSizes] = useState<Sizes | null>(null);
-  const [ctx, setCtx] = useState<CanvasContext | null>(null);
+  const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
   const bitmapWidth = bitmap.width;
   const bitmapHeight = bitmap.height;
@@ -168,8 +72,8 @@ export const BitmapView = ({ bitmap, onChangeBitmap, eraser }: BitmapViewProps):
 
     clearDisplay(ctx, sizes);
     drawBitmap(ctx, bitmap);
-    drawGrid(ctx, sizes);
-    drawSeparators(ctx, sizes, grid);
+    drawPointsBorders(ctx, sizes);
+    drawGrid(ctx, sizes, grid);
   }, [bitmap, ctx, grid, sizes]);
 
   return <canvas ref={setCanvasRef} className={styles.container} onClick={handleClick} onMouseMove={handleMouseMove} />;
