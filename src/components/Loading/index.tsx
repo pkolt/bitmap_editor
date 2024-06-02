fdbf3f44de3393328f34686eb4a879eb1c5c6752
pkolt <pkoltyshev@gmail.com>
2024-06-02 18:33:12 +0400

    Fix refactoring

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,14 @@
+import cn from 'classnames';
+import styles from './index.module.css';
+import { useTranslation } from 'react-i18next';
+
+export const Loading = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      <div className={cn('spinner-border', styles.spinner)} role="status">
+        <span className="visually-hidden">{t('Loading')}...</span>
+      </div>
+    </div>
+  );
+};
--- src/components/SuspenseFallback/index.tsx
+++ src/components/SuspenseFallback/index.tsx
@@ -1,14 +1,5 @@
-import cn from 'classnames';
-import styles from './index.module.css';
-import { useTranslation } from 'react-i18next';
+import { Loading } from '../Loading';
 
 export const SuspenseFallback = () => {
-  const { t } = useTranslation();
-  return (
-    <div className="d-flex justify-content-center align-items-center">
-      <div className={cn('spinner-border', styles.spinner)} role="status">
-        <span className="visually-hidden">{t('Loading')}...</span>
-      </div>
-    </div>
-  );
+  return <Loading />;
 };
