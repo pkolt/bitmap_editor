11e1a31e6dbcefb4b3bc54be51c9fdf7bfa07970
pkolt <pkoltyshev@gmail.com>
2024-06-05 20:55:45 +0400

    Fix link

--- src/pages/Home/components/ButtonList/index.tsx
+++ src/pages/Home/components/ButtonList/index.tsx
@@ -1,23 +1,31 @@
 import { PageUrl } from '@/constants/urls';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export const ButtonList = () => {
   const { t } = useTranslation();
   return (
-    <div className="d-flex flex-column gap-3">
+    <div className="d-flex flex-column gap-3 w-25">
       <Link to={PageUrl.CreateBitmap} className="btn btn-primary btn-lg">
-        {t('Create new bitmap')}
+        {t('Create')}
       </Link>
-      <Link to={PageUrl.ImportFromImage} className="btn btn-primary btn-lg">
-        {t('Import from image')}
-      </Link>
-      <Link to={PageUrl.ImportFromCollections} className="btn btn-primary btn-lg">
-        {t('Import from collections')}
-      </Link>
-      <Link to={PageUrl.ImportFromJson} className="btn btn-primary btn-lg">
-        {t('Import from JSON')}
-      </Link>
+      <Dropdown align="end">
+        <Dropdown.Toggle size="lg" className="w-100">
+          {t('Import')}
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item as={Link} to={PageUrl.ImportFromImage}>
+            {t('From image')}
+          </Dropdown.Item>
+          <Dropdown.Item as={Link} to={PageUrl.ImportFromCollections}>
+            {t('From collections')}
+          </Dropdown.Item>
+          <Dropdown.Item as={Link} to={PageUrl.ImportFromJson}>
+            {t('From JSON')}
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   );
 };
